e6f5956e2e47eee1e5daaa87f1c9db7068ba7823
Michael Beebower <michael.beebower@g.austincc.edu>
2019-03-18 00:01:45 -0500

    Update for_loop.cpp

--- C++/for_loop.cpp
+++ C++/for_loop.cpp
@@ -1,33 +1,39 @@
-#include <bits/stdc++.h>    // numeric_limits<streamsize>
-#include <iostream>         // std::cout
+/*
+https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem
+
+Input Format
+------------
+You will be given two positive integers, a and b (a <= b), separated by a newline.
+
+Output Format
+-------------
+For each integer n in the interval |a,b| :
+
+If 1 <= n <= 9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
+Else if n > 9 and it is an even number, then print "even".
+Else if n > 9 and it is an odd number, then print "odd".
+*/
+
+#include <cstdio>
+#include <iostream>     // std::cout
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+int main() {
+    int a,b;
+    cin >> a;
+    cin >> b;
+    string num[9] = {
+      "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-    if (n == 1)
-        cout << "one";
-    else if (n == 2)
-        cout << "two";
-    else if (n == 3)
-        cout << "three";
-    else if (n == 4)
-        cout << "four";
-    else if (n == 5)
-        cout << "five";
-    else if (n == 6)
-        cout << "six";
-    else if (n == 7)
-        cout << "seven";
-    else if (n == 8)
-      cout << "eight";
-    else if (n == 9)
-      cout << "nine";
-    else
-      cout << "Greater than 9";
+    for (int i = a; i <= b; i++)
+    {
+        if (i <= 9)
+            cout << num[i-1] << endl;
+        else if (i % 2 == 0)
+            cout << "even\n";
+        else
+            cout << "odd\n";
+    }
 
     return 0;
 }
