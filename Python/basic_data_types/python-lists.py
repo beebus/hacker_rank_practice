06d85166b4f7926f71b5c59bf279811d7d0aa0e3
Michael Beebower <michael.beebower@g.austincc.edu>
2019-03-22 02:02:38 -0500

    Create python-lists.py

---
+++ Python/basic_data_types/python-lists.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-lists/problem
+if __name__ == '__main__':
+    list = []
+    N = int(input())
+    for x in range(0, N):
+        command_string = str(input())
+        
