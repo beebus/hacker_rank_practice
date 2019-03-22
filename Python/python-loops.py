a72503411e17be6bab29715be6fb283a9db228f1
Michael Beebower <michael.beebower@g.austincc.edu>
2019-03-22 00:54:38 -0500

    Create python-loops.py

---
+++ Python/python-loops.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-loops/problem
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    i = 0
+    while (i < n):
+        print i ** 2
+        i += 1
