7af1206d7b187dec3cbfc9744e0b1cfdcf8d39ab
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-02 07:10:41 -0500

    Update python-lists.py

--- Python/basic_data_types/python-lists.py
+++ Python/basic_data_types/python-lists.py
@@ -1,7 +1,18 @@
 # https://www.hackerrank.com/challenges/python-lists/problem
-if __name__ == '__main__':
+def main():
     list = []
     N = int(input())
     for x in range(0, N):
-        command_string = str(input())
-        
+        s = input().split()
+        cmd = s[0]
+        args = s[1:]
+        if cmd != "print":
+            cmd += "("+ ",".join(args) +")"
+            eval("list."+cmd)
+        else:
+            print(list)
+
+if __name__ == '__main__':
+    main()
+else:
+    pass
