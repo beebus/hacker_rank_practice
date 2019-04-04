97934f4eb323a539cbc36ee6868736a06c141e5c
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-04 04:42:07 -0500

    Create string-validators.py

---
+++ Python/strings/string-validators.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/string-validators/problem
+
+if __name__ == '__main__':
+    s = input()
+    for test in ('isalnum', 'isalpha', 'isdigit', 'islower', 'isupper'):
+        if any(eval("c." + test + "()") for c in s):
+            print("True")
+        else:
+            print("False")
