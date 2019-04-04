e16096b34b7ad996768c1ac3eb891a3be78e5309
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-04 05:30:49 -0500

    Create designer-door-mat.py

---
+++ Python/strings/designer-door-mat.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/designer-door-mat/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+if __name__ == '__main__':
+    m, n = input().split()
