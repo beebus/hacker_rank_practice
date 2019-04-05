f33efd340b2276a16a5ceec21cd374ce40db50b6
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-05 01:34:21 -0500

    Create revising-aggregations-sum.sql

---
+++ SQL/aggregation/revising-aggregations-sum.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
