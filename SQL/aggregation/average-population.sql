8f22e3bd11cae401ded598dfe540856f2fc5d323
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-05 01:42:07 -0500

    Create average-population.sql

---
+++ SQL/aggregation/average-population.sql
@@ -0,0 +1,8 @@
+--https://www.hackerrank.com/challenges/average-population/problem
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT AVG(POPULATION)
+FROM CITY;
