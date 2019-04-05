8e4b6b75177cf21a89b57b3e9a6a9006eff831a3
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-05 02:55:07 -0500

    Create weather-observation-station-13.sql

---
+++ SQL/aggregation/weather-observation-station-13.sql
@@ -0,0 +1,10 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-13/problem
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT CONVERT(decimal(10,4),ROUND(SUM(LAT_N),4,4))
+FROM STATION
+WHERE LAT_N > 38.7880
+    AND LAT_N < 137.2345;
