0c52aafb140ee4d02992424ff891fbf737cbf09e
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-05 01:38:58 -0500

    Create revising-aggregations-the-average-function.sql

---
+++ SQL/aggregation/revising-aggregations-the-average-function.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
