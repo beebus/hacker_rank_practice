bd5d2bae51b9a406569cf96c20826963634897ce
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-05 01:31:39 -0500

    Create revising-aggregations-the-count-function.sql

---
+++ SQL/aggregation/revising-aggregations-the-count-function.sql
@@ -0,0 +1,8 @@
+--https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT COUNT(*)
+FROM CITY
+WHERE POPULATION > 100000;
