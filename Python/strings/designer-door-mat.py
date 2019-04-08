7c4273c34e0b66a85ab3d89740125f0b29fa484a
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-08 02:44:58 -0500

    Update designer-door-mat.py

--- Python/strings/designer-door-mat.py
+++ Python/strings/designer-door-mat.py
@@ -4,4 +4,7 @@
 
 
 if __name__ == '__main__':
-    m, n = input().split()
+    n, m = map(int, input().split())
+    pattern = [('.|.' * (2 * i + 1)).center(m, '-') for i in range(n // 2)]
+    print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
+
