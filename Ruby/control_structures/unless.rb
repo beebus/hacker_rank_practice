7a3a52d85d4eaf13721490da89541501ec73f7dd
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-08 21:35:58 -0500

    Create unless.rb

---
+++ Ruby/control_structures/unless.rb
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/ruby-tutorial-unless/problem
+
+def scoring(array)
+  # update_score of every user in the array unless the user is admin
+  array.each { |user| user.update_score unless user.is_admin? }
+end
