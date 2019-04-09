46e6ae7413750a90407a53a28fb15c64d0e23891
Michael Beebower <michael.beebower@g.austincc.edu>
2019-04-08 21:26:34 -0500

    Create each.rb

---
+++ Ruby/control_structures/each.rb
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/ruby-tutorial-each/problem
+
+def scoring(array)
+    # iterate through each of the element in array using *each* and call update_score on it
+end
