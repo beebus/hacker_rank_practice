b079f43979af04ee0ad64a986fb692c781cd7601
Michael Beebower <michael.beebower@g.austincc.edu>
2019-09-21 07:00:31 -0500

    solved string-formatting.py

--- Python/strings/string-formatting.py
+++ Python/strings/string-formatting.py
@@ -1,8 +1,13 @@
 # https://www.hackerrank.com/challenges/python-string-formatting/problem
 
+
 def print_formatted(number):
-    for x in range(1, number):
-        print(x, oct(x), hex(x), bin(x))
+    width = len(bin(number)) - 2
+    for x in range(1, number+1):
+        print(str(x).rjust(width),
+              str(oct(x)[2:]).rjust(width),
+              str(hex(x)[2:]).upper().rjust(width),
+              str(bin(x)[2:]).rjust(width))
 
 
 if __name__ == '__main__':
