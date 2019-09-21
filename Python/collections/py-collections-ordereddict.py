663d6ac3a820a0ea8480bc3aa43b0783c2ecb0d7
Michael Beebower <michael.beebower@g.austincc.edu>
2019-09-21 13:36:34 -0500

    couple

---
+++ Python/collections/py-collections-ordereddict.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/py-collections-ordereddict/problem
+
+from collections import OrderedDict
+
+d = OrderedDict()
+for _ in range(int(input())):
+    item, space, quantity = input().rpartition(' ')
+    d[item] = d.get(item, 0) + int(quantity)
+for item, quantity in d.items():
+    print(item, quantity)
